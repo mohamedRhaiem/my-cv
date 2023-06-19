d76dd2e4427d0b6ab36ad5696137544d83d49480
mohamed ben rhaiem <mohamedbenrhaiem@mohameds-MacBook-Pro.local>
2023-06-19 00:24:52 -0300

    update resume data

--- Webdir/app/js/controllers.js
+++ Webdir/app/js/controllers.js
@@ -2,13 +2,10 @@ console.log('Loading controllers.js');
 
 var resumeControllers = angular.module('resumeControllers', []);
 
-resumeControllers.controller('bodyCtrl', function ($scope, $http,$localStorage,$document) {
+resumeControllers.controller('bodyCtrl', function ($scope, $http) {
     $http.get('/data/resume.json').then(function(data) {
         $scope.resume = data.data;
     });
-/*   $http.get('https://drive.google.com/file/d/0BwGBt-SB7XTycjAzUFNPMHJDZTg/view').then(function(data) {
-        $scope.resume = data.data;
-    });*/
    
 });
 
